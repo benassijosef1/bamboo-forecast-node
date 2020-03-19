af51a36422dbd2a006d50f9650f48fab19173d77
benassijosef1 <53522878+benassijosef1@users.noreply.github.com>
2020-03-19 16:17:34 +0000

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 module.exports = {
   keys: {
     forecastAuthToken:
-      "Bearer 2230528.at.SbbxCGsXcb1VTUQtmvPmyzWM7X2BQtoZagHoLpPr0T_3qhZ8B5mGQQhak1R9EuOvb3p4zfjMOlm58v0kQnEzkg",
-    forecastAccountId: "1246251",
-    projectId: "2468940"
+      "***********************************************",
+    forecastAccountId: "*******************************",
+    projectId: "******************************************"
   },
   urls: {
     bambooIcal:
-      "http://zaizi.bamboohr.com/feeds/feed.php?id=9bc86de31f5162c30b88f88d1fe4a425",
+      "*********************************************",
     urlTimeOffPut: "https://api.forecastapp.com/assignments",
     urlPeople: "https://api.forecastapp.com/people"
   },
@@ -17,9 +17,4 @@ module.exports = {
   }
 };
 
-// Kaps Details
 
-// forecastAuthToken:
-// "Bearer 1634237.pt.lmMVrMiukzHIFzHW8vVHaR5FOWLuopIr3slt3HLcUIUdlo9PTOhoVci9N3KDVLOt1Yrr8uuFgisNlVzQBGofIw",
-// forecastAccountId: "795781",
-// projectId: "1078238"
